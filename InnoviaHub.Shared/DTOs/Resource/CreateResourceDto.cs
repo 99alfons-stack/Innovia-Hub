24beaf949419ee388d68d41ecaf319f34b34e7a3
phuusie <vanphunguyen@me.com>
2026-09-09 12:22:14 +0200

    addresourcezones

--- InnoviaHub.Shared/DTOs/Resource/CreateResourceDto.cs
+++ InnoviaHub.Shared/DTOs/Resource/CreateResourceDto.cs
@@ -1,8 +1,11 @@
-﻿namespace InnoviaHub.Shared.DTOs.Resource;
+﻿using InnoviaHub.Shared.Enums;
+
+namespace InnoviaHub.Shared.DTOs.Resource;
 
 public class CreateResourceDto
 {
     public string Name { get; set; }
     public Guid ResourceTypeId { get; set; }
+    public ResourceZone? Zone { get; set; } 
     public int Capacity { get; set; }
 }
